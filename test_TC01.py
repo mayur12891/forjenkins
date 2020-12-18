80c4cdce32310aca2cb598d75f004f2b2642d5cc
mayur <mayur12891@gmail.com>
2020-12-18 15:56:07 +0530

    commit1

---
+++ test_TC01.py
@@ -0,0 +1,11 @@
+import time
+
+from selenium import webdriver
+
+def test_TC01():
+    driver = webdriver.Chrome(executable_path="G:\Python Selenium (Mar2020)\Setup exe files\chromedriver.exe")
+    driver.maximize_window()
+    #driver.minimize_window()
+
+    driver.get("https://www.google.com")
+    time.sleep(100)
