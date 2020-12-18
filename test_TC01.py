5488b77aa18cea134b8e75fc9884519781363df3
mayur12891 <35452496+mayur12891@users.noreply.github.com>
2020-12-18 19:49:38 +0530

    Update test_TC01.py

--- test_TC01.py
+++ test_TC01.py
@@ -6,6 +6,7 @@ def test_TC01():
     driver = webdriver.Chrome(executable_path="G:\Python Selenium (Mar2020)\Setup exe files\chromedriver.exe")
     driver.maximize_window()
     #driver.minimize_window()
-
+    a = 3
+    assert a == 4, "Failing this on purpose"
     driver.get("https://www.google.com")
     time.sleep(10)
